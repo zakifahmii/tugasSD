b8a0129a3f2e5365aa91eb6a4fa2c2a04d3c78e6
zakifahmi <73203819+zakifahmii@users.noreply.github.com>
2023-06-26 14:44:04 +0700

    Update SDtugas.py

--- SDtugas.py
+++ SDtugas.py
@@ -4,22 +4,22 @@ import sys as prog
 
 def garis():
     print("------------------------------------------------")
-    
+
 def main():
     print("================================================")
     print("                 Program Matriks")
     print("            Mata Kuliah Struktur Data")
     print("================================================")
-    
+
     print("Pilihlah program yang akan kamu lakukan")
     print("1. Triangular Upper\n2. Triangular Lower\n3. Sparse Array")
     pilih = input("Pilih (1/2/3): ")
-    
+
     garis()
     if pilih == "1":
         def ut(n):
             arr = [[0] * n for _ in range(n)]
-            num = 1 
+            num = 1
             for a in range(n):
                 for b in range(a, n):
                     arr[a][b] = num
@@ -34,24 +34,96 @@ def main():
         for upper in hasil:
             print(upper)
 
+        lanjut = input("Ingin menggabungkan dengan lower triangular? (y/n): ")
+        if lanjut == "y" or lanjut == "Y":
+            def lt(n):
+                arr = [[0] * n for _ in range(n)]
+                num = 1
+                for a in range(n):
+                    for b in range(a + 1):
+                        arr[a][b] = num
+                        num += 1
+                return arr
+
+            n = int(input("Masukkan Banyak Elemen: "))
+            garis()
+            hasil = lt(n)
+            print("Lower Trigular Array")
+
+            for lower in hasil:
+                print(lower)
+            garis()
+            print("Gabungan Upper dan Lower Triangular")
+            def merge_triangular(upper, lower):
+                merged = []
+                for i in range(len(upper)):
+                    row = lower[i] + upper[i]
+                    merged.append(row)
+                
+                return merged
+            
+            upper_matriks = ut(n)
+            lower_matriks = lt(n)
+            merged_matriks = merge_triangular(upper_matriks, lower_matriks)
+            
+            merged_rums = [[elemen for elemen in row if elemen != 0] for row in merged_matriks]
+            for row in merged_rums:
+                print(row)
+                
     elif pilih == "2":
-        def ut(n):
+        def lt(n):
             arr = [[0] * n for _ in range(n)]
-            num = 1 
+            num = 1
             for a in range(n):
                 for b in range(a + 1):
                     arr[a][b] = num
                     num += 1
             return arr
-        
+
         n = int(input("Masukkan Banyak Elemen: "))
         garis()
-        hasil = ut(n)
+        hasil = lt(n)
         print("Lower Trigular Array")
 
         for lower in hasil:
             print(lower)
+            
+        lanjut = input("Ingin menggabungkan dengan upper triangular? (y/n): ")
+        if lanjut == "y" or lanjut == "Y":
+            def ut(n):
+                arr = [[0] * n for _ in range(n)]
+                num = 1
+                for a in range(n):
+                    for b in range(a, n):
+                        arr[a][b] = num
+                        num += 1
+                return arr
 
+            n = int(input("Masukkan Banyak Elemen: "))
+            garis()
+            hasil = ut(n)
+            print("Upper Trigular Array")
+            for upper in hasil:
+                print(upper)
+                
+            garis()
+            print("Gabungan Upper dan Lower Triangular")
+            def merge_triangular(upper, lower):
+                merged = []
+                for i in range(len(upper)):
+                    row = lower[i] + upper[i]
+                    merged.append(row)
+                
+                return merged
+            
+            upper_matriks = ut(n)
+            lower_matriks = lt(n)
+            merged_matriks = merge_triangular(upper_matriks, lower_matriks)
+            
+            merged_rums = [[elemen for elemen in row if elemen != 0] for row in merged_matriks]
+            for row in merged_rums:
+                print(row)
+        
     elif pilih == "3":
         class SparseArray:
             def __init__(self, rows, cols):
@@ -71,13 +143,14 @@ def main():
                     for j in range(self.cols):
                         print(self.get_value(i, j), end=' ')
                     print()
-        
+
         x = int(input("Masukkan Banyak Elemen Baris: "))
         y = int(input("Masukkan Banyak Elemen Kolom: "))
-        spArr = SparseArray(x,y)
+        spArr = SparseArray(x, y)
         garis()
         print("Masukkan 3 angka kedalam 4 value!")
         matriks = []
+        
         for i in range(x):
             row = []
             for j in range(y):
@@ -89,18 +162,32 @@ def main():
         print("Matriks yang diinput:")
         for row in matriks:
             print(row)
+        garis()
         
-        # spArr.set_value(matriks)
-        spArr.print_array()
-        
-while True: 
-    main() 
+        print("Sparse Array")
+        jumlah_0 = 0
+        jumlah_data_selain_0 = 0
+
+        for row in matriks:
+            for elemen in row:
+                if elemen == 0:
+                    jumlah_0 += 1
+                else:
+                    jumlah_data_selain_0 += 1
+
+        print("Jumlah data 0 sebanyak:", jumlah_0)
+        print("Jumlah data selain 0 sebanyak:", jumlah_data_selain_0)
+
+
+while True:
+    main()
     ulang = input("\nIngin membuat matrix lagi?(Y/N): ")
     if ulang == 'Y' or ulang == 'y':
         print("\n")
-        continue 
+        continue
     elif ulang == 'N' or ulang == 'n':
-        prog.exit() 
-    else: 
+        print("Terima Kasih")
+        prog.exit()
+    else:
         print("Ada Kesalahan!")
-        prog.exit() 
+        prog.exit()
